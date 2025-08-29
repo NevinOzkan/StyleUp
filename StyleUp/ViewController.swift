bd1a85549aa3ecef95bf40572347d59a55549d6b
Nevin Ozkan <ozkannevin73@gmail.com>
2025-08-29 15:46:15 +0300

    Initial Commit

---
+++ StyleUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StyleUp
+//
+//  Created by Nevin Özkan on 29.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StyleUpTests/StyleUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  StyleUpTests.swift
+//  StyleUpTests
+//
+//  Created by Nevin Özkan on 29.08.2025.
+//
+
+import Testing
+@testable import StyleUp
+
+struct StyleUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
